35c75bbb9751110219c8b00afaf7141b95ee4f77
Cesar Augusto Manholer <36235480+CyberPh4nt0m@users.noreply.github.com>
2020-10-15 08:49:18 -0300

    1.0

--- photoshare/seguir.php
+++ photoshare/seguir.php
@@ -13,12 +13,12 @@ while($seguindo = mysqli_fetch_array($query_seguindo)){
     <head>
         <title>Photo Share</title>
         <meta charset="utf-8">
-        <link rel="stylesheet" href="css/seguir.css">
+        <link rel="stylesheet" href="css/seguir.min.css">
         <link rel="stylesheet" href="css/bootstrap.min.css">
         <link rel="shortcut icon" href="icon.svg"> <!-- Favicon -->
-        <script src="JavaScript/jquery.min.js"></script>
-        <script src="JavaScript/pooper.min.js"></script>
-        <script src="JavaScript/bootstrap.min.js"></script>
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
     </head>
     <body>
